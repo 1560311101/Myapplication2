4c0ec3db8f9dcc24fedafccabfe674dd394a92b6
1560311101 <1902139457@qq.com>
2017-11-10 10:03:46 +0800

    11.10

---
+++ app/src/main/java/com/example/administrator/myapplication/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.administrator.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button button = (Button)findViewById(R.id.Login_TextView_Send);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
